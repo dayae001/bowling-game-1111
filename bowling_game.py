c67f707ce06059947b8808a7de3ffb868e23a610
dayae001 <dayae001@naver.com>
2024-06-13 11:25:10 +0900

    [refactoring] test_샘플게임

--- bowling_game.py
+++ bowling_game.py
@@ -1,30 +1,38 @@
 class BowlingGame:
     def __init__(self):
-        self.score_board = []
+        self.rolls = []
 
     def roll(self, pins):
-        if pins == 10:
-            self.score_board.append(pins)
-            self.score_board.append(0)
-        else:
-            self.score_board.append(pins)
+        self.rolls.append(pins)
 
     def score(self):
-        rst = sum(self.score_board)
+        total_score = 0
+        roll_index = 0
 
-        for i in range(len(self.score_board) // 2 - 1):
-            if (self.score_board[i * 2] != 10 and self.score_board[i * 2 + 1] != 10) and (
-                    self.score_board[i * 2] + self.score_board[i * 2 + 1] == 10):
-                rst += self.score_board[(i + 1) * 2]
+        for frame in range(10):
+            if self.is_strike(roll_index):  # 스트라이크
+                total_score += 10 + self.strike_bonus(roll_index)
+                roll_index += 1
+            elif self.is_spare(roll_index):  # 스페어
+                total_score += 10 + self.spare_bonus(roll_index)
+                roll_index += 2
+            else:  # 일반 프레임
+                total_score += self.sum_of_balls_in_frame(roll_index)
+                roll_index += 2
 
-            if (self.score_board[i * 2] == 10 or self.score_board[i * 2 + 1] == 10):
-                if i >= 9:
-                    continue
-                if self.score_board[(i + 1) * 2] != 10:
-                    rst += self.score_board[(i + 1) * 2]
-                    rst += self.score_board[(i + 1) * 2 + 1]
-                else:
-                    rst += self.score_board[(i + 1) * 2]
-                    rst += self.score_board[(i + 2) * 2]
+        return total_score
 
-        return rst
+    def is_strike(self, roll_index):
+        return self.rolls[roll_index] == 10
+
+    def strike_bonus(self, roll_index):
+        return self.rolls[roll_index + 1] + self.rolls[roll_index + 2]
+
+    def is_spare(self, roll_index):
+        return self.rolls[roll_index] + self.rolls[roll_index + 1] == 10
+
+    def spare_bonus(self, roll_index):
+        return self.rolls[roll_index + 2]
+
+    def sum_of_balls_in_frame(self, roll_index):
+        return self.rolls[roll_index] + self.rolls[roll_index + 1]
